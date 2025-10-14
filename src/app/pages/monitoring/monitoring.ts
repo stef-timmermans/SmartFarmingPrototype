6a601aff99a4e0f6d6cfc80a69c24dcfe178776d
Stef Timmermans <stef.timmermans.pdx@gmail.com>
2025-10-14 12:56:02 +0200

    Basic dropdowns for sensors view

--- src/app/pages/monitoring/monitoring.ts
+++ src/app/pages/monitoring/monitoring.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import {NzListComponent, NzListItemComponent} from 'ng-zorro-antd/list';
+import {NzCollapseComponent, NzCollapsePanelComponent} from 'ng-zorro-antd/collapse';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-monitoring',
-  imports: [],
+  imports: [
+    NzListComponent,
+    NzListItemComponent,
+    NzCollapsePanelComponent,
+    NgForOf,
+    NzCollapseComponent
+  ],
   templateUrl: './monitoring.html',
   styleUrl: './monitoring.css'
 })
