7628fb4b44e26370e19d46c384c16f3811e32cd5
Stef Timmermans <stef.timmermans.pdx@gmail.com>
2025-10-15 14:01:36 +0200

    Basic chart

--- src/app/pages/monitoring/monitoring.ts
+++ src/app/pages/monitoring/monitoring.ts
@@ -1,18 +1,45 @@
 import { Component } from '@angular/core';
-import {NzListComponent, NzListItemComponent} from 'ng-zorro-antd/list';
-import {NzCollapseComponent, NzCollapsePanelComponent} from 'ng-zorro-antd/collapse';
+import { NzListComponent, NzListItemComponent } from 'ng-zorro-antd/list';
+import { NzCollapseComponent, NzCollapsePanelComponent } from 'ng-zorro-antd/collapse';
+import { NgApexchartsModule, ApexAxisChartSeries, ApexChart, ApexXAxis, ApexStroke } from 'ng-apexcharts';
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  stroke: ApexStroke;
+};
 
 @Component({
   selector: 'app-monitoring',
+  standalone: true,
   imports: [
     NzListComponent,
     NzListItemComponent,
     NzCollapsePanelComponent,
-    NzCollapseComponent
+    NzCollapseComponent,
+    NgApexchartsModule
   ],
   templateUrl: './monitoring.html',
   styleUrl: './monitoring.css'
 })
 export class Monitoring {
-
+  public chartOptions: ChartOptions = {
+    series: [
+      {
+        name: 'Moisture',
+        data: [10, 41, 35, 51, 49, 62, 69]
+      }
+    ],
+    chart: {
+      type: 'line',
+      height: 200
+    },
+    stroke: {
+      curve: 'smooth'
+    },
+    xaxis: {
+      categories: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    }
+  };
 }
