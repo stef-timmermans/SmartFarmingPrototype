05ee7bce17747d1118523c610984c012f2c4202f
Stef Timmermans <stef.timmermans.pdx@gmail.com>
2025-11-06 21:25:22 +0100

    Basic form for fertilizer tasks

--- src/app/pages/tasks/tasks.ts
+++ src/app/pages/tasks/tasks.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { NgFor, NgIf } from '@angular/common';
+import {NgClass, NgFor, NgIf} from '@angular/common';
 import { NzListComponent, NzListItemComponent } from 'ng-zorro-antd/list';
 import { NzCollapseComponent, NzCollapsePanelComponent } from 'ng-zorro-antd/collapse';
 import { NzButtonComponent } from 'ng-zorro-antd/button';
 import { NzInputDirective } from 'ng-zorro-antd/input';
 import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzSelectModule } from 'ng-zorro-antd/select';
 
 @Component({
   selector: 'app-tasks',
@@ -20,31 +21,41 @@ import { NzFormModule } from 'ng-zorro-antd/form';
     NzCollapseComponent,
     NzCollapsePanelComponent,
     NzButtonComponent,
-    NzInputDirective
+    NzInputDirective,
+    NzSelectModule,
+    NgClass
   ],
   templateUrl: './tasks.html',
   styleUrl: './tasks.css'
 })
 export class Tasks {
   fertilizerTasks = [
-    { name: 'Fertilizing – Valve #1', time: '08:00' },
-    { name: 'Fertilizing – Valve #2', time: '10:30' },
-    { name: 'Fertilizing – Valve #3', time: '13:00' }
+    { name: 'Fertilizing – Valve #1', time: '08:00', status: 'completed' },
+    { name: 'Fertilizing – Valve #2', time: '10:30', status: 'completed' },
+    { name: 'Fertilizing – Valve #3', time: '13:00', status: 'completed' }
   ];
 
   showAddTaskForm = false;
+  newTaskType = '';
   newTaskName = '';
   newTaskTime = '';
 
-  addTask() {
-    this.fertilizerTasks.push({
-      name: this.newTaskName,
-      time: this.newTaskTime
-    });
+  get fertilizerPendingCount(): number {
+    return this.fertilizerTasks.filter(t => t.status === 'scheduled').length;
+  }
 
-    // reset form
-    this.newTaskName = '';
-    this.newTaskTime = '';
-    this.showAddTaskForm = false;
+  addTask() {
+    if (this.newTaskName && this.newTaskTime) {
+      this.fertilizerTasks.push({
+        name: this.newTaskName,
+        time: this.newTaskTime,
+        status: 'scheduled'
+      });
+
+      this.newTaskType = '';
+      this.newTaskName = '';
+      this.newTaskTime = '';
+      this.showAddTaskForm = false;
+    }
   }
 }
