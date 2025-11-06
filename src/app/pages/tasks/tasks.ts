629b1448dc43b51a1b27f5f64b4dce59d04eeb9b
Stef Timmermans <stef.timmermans.pdx@gmail.com>
2025-11-06 19:33:11 +0100

    basic form

--- src/app/pages/tasks/tasks.ts
+++ src/app/pages/tasks/tasks.ts
@@ -1,20 +1,50 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgFor, NgIf } from '@angular/common';
 import { NzListComponent, NzListItemComponent } from 'ng-zorro-antd/list';
 import { NzCollapseComponent, NzCollapsePanelComponent } from 'ng-zorro-antd/collapse';
-import { NzTagComponent } from 'ng-zorro-antd/tag';
-import { NgClass } from '@angular/common';
+import { NzButtonComponent } from 'ng-zorro-antd/button';
+import { NzInputDirective } from 'ng-zorro-antd/input';
+import { NzFormModule } from 'ng-zorro-antd/form';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
   imports: [
+    FormsModule,
+    NgFor,
+    NgIf,
+    NzFormModule,
     NzListComponent,
     NzListItemComponent,
     NzCollapseComponent,
     NzCollapsePanelComponent,
-    NzTagComponent
+    NzButtonComponent,
+    NzInputDirective
   ],
   templateUrl: './tasks.html',
   styleUrl: './tasks.css'
 })
-export class Tasks {}
+export class Tasks {
+  fertilizerTasks = [
+    { name: 'Fertilizing – Valve #1', time: '08:00' },
+    { name: 'Fertilizing – Valve #2', time: '10:30' },
+    { name: 'Fertilizing – Valve #3', time: '13:00' }
+  ];
+
+  showAddTaskForm = false;
+  newTaskName = '';
+  newTaskTime = '';
+
+  addTask() {
+    this.fertilizerTasks.push({
+      name: this.newTaskName,
+      time: this.newTaskTime
+    });
+
+    // reset form
+    this.newTaskName = '';
+    this.newTaskTime = '';
+    this.showAddTaskForm = false;
+  }
+}
