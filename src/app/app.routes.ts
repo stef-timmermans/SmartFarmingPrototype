39343c4209d352185bd5d4ed2c3f3800bdb093ea
Stef Timmermans <stef.timmermans.pdx@gmail.com>
2025-11-06 19:03:18 +0100

    remove irrigation; basic skeleton tasks page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
 import { Home } from './pages/home/home';
 import { Monitoring} from './pages/monitoring/monitoring';
-import { Irrigation} from './pages/irrigation/irrigation';
 import { Tasks } from './pages/tasks/tasks';
 
 export const routes: Routes = [
@@ -10,7 +9,6 @@ export const routes: Routes = [
   { path: 'login', component: Login },
   { path: 'home', component: Home },
   { path: 'monitoring', component: Monitoring },
-  { path: 'irrigation', component: Irrigation },
   { path: 'tasks', component: Tasks },
   { path: '**', redirectTo: 'login' }
 ];
--- src/app/pages/tasks/tasks.ts
+++ src/app/pages/tasks/tasks.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { NzListComponent, NzListItemComponent } from 'ng-zorro-antd/list';
+import { NzCollapseComponent, NzCollapsePanelComponent } from 'ng-zorro-antd/collapse';
+import { NzTagComponent } from 'ng-zorro-antd/tag';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-tasks',
-  imports: [],
+  standalone: true,
+  imports: [
+    NzListComponent,
+    NzListItemComponent,
+    NzCollapseComponent,
+    NzCollapsePanelComponent,
+    NzTagComponent
+  ],
   templateUrl: './tasks.html',
   styleUrl: './tasks.css'
 })
-export class Tasks {
-
-}
+export class Tasks {}
